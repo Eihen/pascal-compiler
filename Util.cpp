54058f6c37b15a2149c83ce60ac69408f8dd8a04
Emerson Belancieri <emersonbelancieri@hotmail.com>
2017-04-12 23:18:45 -0300

    Implementação da função isKeyword;

--- Util.cpp
+++ Util.cpp
@@ -92,7 +92,11 @@ bool Util::isFloat(string s) {
 }
 
 bool Util::isKeyword(string s) {
-    return true;
+	set<string> keywords = {"and", "array", "asm", "begin", "break", "case", "const", "constructor", "continue", "destructor", "div", "do", "downto", "else", 
+						 "end", "false", "file", "for", "function", "goto", "if", "implementation", "in", "inline", "interface", "label", "mod", "nil", "not", 
+						 "object", "of", "on", "operator", "or", "packed", "procedure", "program", "record", "repeat", "set", "shl", "shr", "string", "then", 
+						 "to", "true", "type", "unit", "until", "uses", "var", "while", "with", "xor"};
+	return keywords.find(s) != keywords.end();
 }
 
 Util::~Util()
