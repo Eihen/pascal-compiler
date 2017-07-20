fc37021b09c3565079d5e13f616f0536a0c2a872
Emerson Belancieri <emersonbelancieri@hotmail.com>
2017-07-20 11:09:57 -0300

    Documentação atualiada com informações sobre o Gerador de Código; Exemplo para ser utilizado para gerar código;

---
+++ Debug/GeraCodigo.pas
@@ -0,0 +1,29 @@
+program exemploGeraCodigo (input, output);
+
+var m,n :  integer;
+    x :  integer;
+    a,b,c,d,e :  integer;
+
+begin
+  n:=25+37+55-38*3-9+(90-15+(25*35))-2;
+
+  if (m < e) then
+    if (a > b) then
+      d:= a - b
+    else
+      d:= b - a
+  else
+    d:=a+b;
+
+  while (5>2) do
+  begin
+    x:=10;
+    while(5>2) do
+    begin
+        x:=10;
+        c:=5
+    end;
+    c:=5
+  end
+end.
+
